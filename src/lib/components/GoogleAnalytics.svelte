2a9e32e7823c4bbf41e7ca47f11e26c284baffb3
kazedevs <businessrajkumardas@gmail.com>
2025-04-20 18:54:54 +0530

    Improve Google Analytics implementation with dedicated component

---
+++ src/lib/components/GoogleAnalytics.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  
+  onMount(() => {
+    // Load Google Analytics script
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-5GPDFL5PPC';
+    script.async = true;
+    document.head.appendChild(script);
+    
+    // Initialize Google Analytics
+    // @ts-ignore
+    window.dataLayer = window.dataLayer || [];
+    // @ts-ignore
+    function gtag(...args: any[]){window.dataLayer.push(arguments);}
+    // @ts-ignore
+    gtag('js', new Date());
+    // @ts-ignore
+    gtag('config', 'G-5GPDFL5PPC');
+  });
+</script> 
